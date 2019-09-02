a56471ed36f9b18867cf0833385b5872e2c5c95b
Vincent Rischmann <vincent@rischmann.fr>
2019-09-03 00:08:06 +0200

    add commands to seal/open boxes for debugging

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -30,10 +31,9 @@ func main() {
 	flag.Usage = func() {
 		fmt.Printf("Usage: apero [option] <command> [option]\n\n")
 		fmt.Printf("Available commands are:\n")
-		fmt.Printf("    genkeys %50s\n", "generate public and private keys")
-		fmt.Printf("    encrypt %50s\n", "encrypt a message")
-		fmt.Printf("    decrypt %50s\n", "decrypt a message")
 		fmt.Printf("    serve %50s\n", "serve the API endpoints")
+		fmt.Printf("    genkeys %50s\n", "generate public and private keys")
+		fmt.Printf("    secretbox %50s\n", "seal and open secret boxes")
 		fmt.Printf("\nOptions are:\n")
 		flag.PrintDefaults()
 		fmt.Println()
@@ -122,6 +122,49 @@ func main() {
 		default:
 			fs.Usage()
 			os.Exit(1)
+		}
+
+	case "secretbox":
+		fs := flag.NewFlagSet("secetbox", flag.ContinueOnError)
+		flKey := fs.String("key", "", "The secret key used to seal/open the box")
+		flOpen := fs.Bool("open", false, "Open a sealed box")
+		flSeal := fs.Bool("seal", false, "Seal a message into a box")
+		if err := fs.Parse(args); err != nil {
+			fs.Usage()
+			os.Exit(1)
+		}
+
+		keyData, err := hex.DecodeString(*flKey)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(keyData) != secretBoxKeySize {
+			log.Fatalf("invalid key size %d", len(keyData))
+		}
+
+		var key secretBoxKey
+		copy(key[:], keyData)
+
+		switch {
+		case *flOpen:
+			data, err := hex.DecodeString(fs.Arg(0))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			message, opened := secretBoxOpen(data, key)
+			if !opened {
+				log.Fatal("unable to open box")
+			}
+
+			fmt.Printf("%s\n", string(message))
+
+		case *flSeal:
+			message := []byte(fs.Arg(0))
+
+			ciphertext := secretBoxSeal(message, key)
+
+			fmt.Printf("%02x\n", ciphertext)
 		}
 	}
 

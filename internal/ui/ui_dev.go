83742d62247182f288dd78af672c6694ff0179d7
Vincent Rischmann <vincent@rischmann.fr>
2019-10-29 02:20:09 +0100

    internal/ui: ParseTemplate takes a funcmap now

--- internal/ui/ui_dev.go
+++ internal/ui/ui_dev.go
@@ -2,15 +2,32 @@
 
 package ui
 
-import "io/ioutil"
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+)
 
 // GetFile returns the content of the file at path
 func GetFile(path string) ([]byte, error) {
 	if path[0] != '/' {
-		panic("path should start with /")
+		panic(fmt.Errorf("path %q should start with /", path))
 	}
 
 	path = "./ui" + path
 
 	return ioutil.ReadFile(path)
+}
+
+// ParseTemplate parses the content of the files in `paths` into a template.
+// If there's any error the function panics.
+func ParseTemplate(funcs template.FuncMap, paths ...string) *template.Template {
+	for i := range paths {
+		if paths[i][0] != '/' {
+			panic(fmt.Errorf("path %q should start with /", paths[i]))
+		}
+		paths[i] = "./ui" + paths[i]
+	}
+
+	return template.Must(template.New("root").Funcs(funcs).ParseFiles(paths...))
 }
--- internal/ui/ui_release.go
+++ internal/ui/ui_release.go
@@ -3,6 +3,9 @@
 package ui
 
 import (
+	"fmt"
+	"html/template"
+
 	_ "rischmann.fr/apero/statik"
 
 	"github.com/rakyll/statik/fs"
@@ -11,7 +14,7 @@ import (
 // GetFile returns the content of the file at path
 func GetFile(path string) ([]byte, error) {
 	if path[0] != '/' {
-		panic("path should start with /")
+		panic(fmt.Errorf("path %q should start with /", path))
 	}
 
 	statikFS, err := fs.New()
@@ -20,4 +23,17 @@ func GetFile(path string) ([]byte, error) {
 	}
 
 	return fs.ReadFile(statikFS, path)
+}
+
+// ParseTemplate parses the content of the files in `paths` into a template.
+// If there's any error the function panics.
+func ParseTemplate(funcs template.FuncMap, paths ...string) *template.Template {
+	tmpl := template.New("root").Funcs(funcs)
+
+	for _, path := range paths {
+		data := MustGetFile(path)
+		tmpl = template.Must(tmpl.Parse(string(data)))
+	}
+
+	return tmpl
 }
